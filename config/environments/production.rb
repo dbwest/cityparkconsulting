4701701380db451f12398397bff48fa0c4b40fcd
David West <david.b.west@gmail.com>
2012-07-19 15:44:00 -0400

    xsendfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Cityparkconsulting::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
